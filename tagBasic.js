8382a06c51c594e0c2c327201b9fad942819f129
Андрей Берлин <99011598+Andrey-Berlin@users.noreply.github.com>
2022-02-12 20:54:34 +0200

    update tagBasic.js

--- tagBasic.js
+++ tagBasic.js
@@ -1,1 +1,14 @@
+let body = document.getElementById("body");
+body.innerHTML = `
+<style id = "style">
+</style>
+<header id="header">
+    
+</header>
+<section id="section">
 
+
+</section>
+<footer id="footer">
+</footer>
+`;
